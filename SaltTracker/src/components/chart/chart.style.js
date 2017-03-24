f953dd43c4147749cd175655bc40f782961f7be3
Michiel de Jong <mdejong@bol.com>
2017-03-24 11:55:37 +0100

    better colors

---
+++ SaltTracker/src/components/chart/chart.style.js
@@ -0,0 +1,30 @@
+export default {
+  bar: {    
+    position: 'absolute',    
+    backgroundColor: '#57ad68',
+    alignItems: 'center',
+    borderRadius: 8,    
+    right: 2,
+    left: 2,
+    bottom: 2,
+    borderWidth: 1,
+    borderColor: '#439f55'    
+  },
+  container: {    
+    position: 'absolute',
+    width: 100,
+    height: 500
+  },  
+  containerBar: {
+    width: 100,
+    height: 400,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    borderRadius: 10
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    padding: 5
+  }
+};
