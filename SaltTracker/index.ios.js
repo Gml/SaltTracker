1c037a624210c70424de31a62b112de3134938b1
Michiel de Jong <mdejong@bol.com>
2017-03-24 13:24:17 +0100

    introduced react navigation

--- SaltTracker/index.ios.js
+++ SaltTracker/index.ios.js
@@ -5,31 +5,46 @@
  */
 
 import React, { Component } from 'react';
+import { StackNavigator } from 'react-navigation';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 import Chart from './src/components/chart/chart.component'
 import ProductList from './src/components/product-list/product-list.component'
 import ProductDao from './src/dao/product.dao'
 
-export default class SaltTracker extends Component {
+class HomeScreen extends Component {
   render() {
+    const { navigate } = this.props.navigation;
+
     return (
       <View style={styles.container}>
-        {/*<View style={styles.container}>*/}
-          {/*<Chart maxIntake="6" currentIntake="6"></Chart>*/}
-        {/*</View>*/}
         <View style={styles.container}>
-          <ProductList dao={ProductDao}></ProductList>
+          <Chart maxIntake="6" currentIntake="6"></Chart>
         </View>
+        <Button
+          onPress={() => navigate('ProductList', {dao: ProductDao })}
+          title="Tomaat"
+        />
+
+
+        {/*<View style={styles.container}>
+          <ProductList aap="joehoe" aapjes={aapjes} dao={ProductDao}></ProductList>
+        </View>*/}
       </View>
     );
   }
 }
+
+const SaltTracker = StackNavigator({
+  Home: { screen: HomeScreen },
+  ProductList: {screen: ProductList}  
+});
 
 const styles = StyleSheet.create({
   container: {
